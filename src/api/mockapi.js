c7382b76aeac18c6b2d45fb2ba85fc8c38ab1b82
omeersari <omeersari@gmail.com>
2021-11-09 22:43:23 +0300

    mock api servise is created

--- src/api/mockapi.js
+++ src/api/mockapi.js
@@ -1,1 +1,27 @@
-// mock api endponits will be here.
+import axios from "axios";
+
+const client = axios.create({
+  baseURL: "https://618a7c5c34b4f400177c4716.mockapi.io/",
+  json: true,
+});
+
+export default {
+  async execute(method, resource, data) {
+    return client({
+      method,
+      url: resource,
+      data,
+    }).then((req) => {
+      return req.data;
+    });
+  },
+  login() {
+    return this.execute("get", "admin");
+  },
+  getAllPassengers() {
+    return this.execute("get", "passengers");
+  },
+  createPassenger(passenger) {
+    return this.execute("post", "passengers", passenger);
+  },
+};
