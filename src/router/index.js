ee94b48f32768924fc6c61a3b0cef85ec8ee20d1
omeersari <omeersari@gmail.com>
2021-11-09 17:51:23 +0300

    router updated.

--- src/router/index.js
+++ src/router/index.js
@@ -11,13 +11,22 @@ const routes = [
     component: Home,
   },
   {
-    path: "/about",
-    name: "About",
+    path: "/passengers",
+    name: "Passengers",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+      import(/* webpackChunkName: "passengers" */ "../views/Passengers.vue"),
+  },
+  {
+    path: "/routedetail",
+    name: "RouteDetail",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "routedetail" */ "../views/RouteDetail.vue"),
   },
 ];
 
